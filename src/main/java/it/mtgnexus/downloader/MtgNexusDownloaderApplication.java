b673c5522fefcfdf0080d126e42232ffc9799ba4
Emmettippi <coseacasorandomiche@gmail.com>
2025-03-08 13:04:26 +0100

    second commit

---
+++ src/main/java/it/mtgnexus/downloader/MtgNexusDownloaderApplication.java
@@ -0,0 +1,13 @@
+package it.mtgnexus.downloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgNexusDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgNexusDownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/mtgnexus/downloader/MtgNexusDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mtgnexus.downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgNexusDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
